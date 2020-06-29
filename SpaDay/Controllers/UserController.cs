e2d4cc85876ddceae86837433f80b1d7ed305267
William Morrison <williamchrismorrison@gmail.com>
2020-06-29 14:34:53 -0700

    studio complete

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SpaDay.Models;
+using SpaDay.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,25 +20,46 @@ namespace SpaDay.Controllers
 
         public IActionResult Add()
         {
-            return View();
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
+
+            return View(addUserViewModel);
         }
 
         [HttpPost]
-        [Route("/user")]
-        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        public IActionResult Add(AddUserViewModel addUserViewModel)
         {
-            if (newUser.Password == verify)
+            if (ModelState.IsValid)
             {
-                ViewBag.user = newUser;
-                return View("Index");
+                if (addUserViewModel.Password == addUserViewModel.VerifyPassword)
+                {
+                    User newUser = new User
+                    {
+                        Username = addUserViewModel.Username,
+                        Email = addUserViewModel.Email,
+                        Password = addUserViewModel.Password
+                    };
+                    return View("Index", newUser);
+                }
+                else
+                {
+                    return View("Add");
+                }
             }
-            else
-            {
-                ViewBag.error = "Passwords do not match! Try again!";
-                ViewBag.userName = newUser.Username;
-                ViewBag.eMail = newUser.Email;
-                return View("Add");
-            }
+
+            return View(addUserViewModel);
+
+            //if (newUser.Password == verify)
+            //{
+            //    ViewBag.user = newUser;
+            //    return View("Index");
+            //}
+            //else
+            //{
+            //    ViewBag.error = "Passwords do not match! Try again!";
+            //    ViewBag.userName = newUser.Username;
+            //    ViewBag.eMail = newUser.Email;
+            //    return View("Add");
+            //}
         }
 
     }
